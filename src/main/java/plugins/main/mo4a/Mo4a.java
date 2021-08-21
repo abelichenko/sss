8553933363b18c508e1daf4ed3fbeddeea3ec02c
abelichenko <62173389+abelichenko@users.noreply.github.com>
2021-08-22 01:02:21 +0300

    Initial commit

---
+++ src/main/java/plugins/main/mo4a/Mo4a.java
@@ -0,0 +1,17 @@
+package plugins.main.mo4a;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mo4a extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
